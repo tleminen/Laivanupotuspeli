7c3133995412857949b32003ddd060c297757367
Tomi Leminen <tleminen@student.uef.fi>
2024-02-19 17:52:07 +0200

    juuu kaikkea kivaa

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const postControllers = require("../controllers/postControllers");
 const router = express.Router();
 
-router.route("/").post(postControllers.createNewPosts); //lisaa
+router.route("/").post(postControllers.createNewPost); //lisaa
 
 router.route("/").get(postControllers.getAllPosts); //hae kaikki
 

d80f04c4bda3c82dd0c47ae9dcca6ac1d1713dc0
Tomi Leminen <tleminen@student.uef.fi>
2024-03-11 17:06:23 +0200

    globalstate

---
+++ frontend/src/context/GlobalState.js
@@ -0,0 +1,74 @@
+import React, { useReducer } from "react";
+import AppReducer from "./AppReducer";
+import KayttajaContext from "./KayttajaContext";
+import { GET_KAYTTAJAT } from "./types";
+import axios from "axios";
+const GlobalState = (props) => {
+  //initial state
+  let initialState = {
+    kayttajat: [],
+  };
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+  const getKayttajat = async () => {
+    try {
+      let res = await axios.get("http://localhost:3000/laivanupotus");
+      let { data } = res;
+      dispatch({ type: GET_KAYTTAJAT, payload: data.posts });
+    } catch (error) {
+      alert(error);
+      console.error(error);
+    }
+  };
+  const getKayttajaid = async (id) => {
+    try {
+      let sql = `http://localhost:3000/laivanupotus/${id}`;
+      let res = await axios.get(sql);
+      let { data } = res;
+      console.log("GET_KAYTTAJAID:");
+      dispatch({ type: "GET_KAYTTAJAID", payload: data.posts });
+      //return data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const setKayttaja = async (uusiKayttaja) => {
+    try {
+      const res = await axios
+        .post(`http://localhost:3000/laivanupotus`, uusiKayttaja)
+        .then((res) => {
+          dispatch({ type: "ADD_KAYTTAJA", payload: res.data });
+          console.log(res.data.posts);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const updateKayttajaPisteet = async (id) => {
+    try {
+      const res = await axios
+        .put(`http://localhost:3000/laivanupotus/${id}`)
+        .then((res) => {
+          dispatch({ type: "EDIT_KAYTTAJA", payload: res.data });
+          console.log(res.data.posts);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  return (
+    <KayttajaContext.Provider
+      value={{
+        kayttajat: state.kayttajat,
+        setKayttaja,
+        getKayttajat,
+        getKayttajaid,
+        updateKayttajaPisteet
+      }}
+    >
+      {props.children}
+    </KayttajaContext.Provider>
+  );
+};
+//};
+export default GlobalState;

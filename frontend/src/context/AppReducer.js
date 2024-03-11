81726c3067ecafe5427395d8c26af510d7c3a584
sauruselmeri <sauruse@uef.fi>
2024-03-11 16:58:54 +0200

    tein appreducerin

---
+++ frontend/src/context/AppReducer.js
@@ -0,0 +1,31 @@
+export default (state, action) => {
+  const { payload, type } = action;
+  switch (type) {
+    case "GET_KAYTTAJAT":
+      return {
+        ...state,
+        kayttajat: payload,
+      };
+    case "GET_KAYTTAJAID":
+      return {
+        ...state,
+        kayttajat: kayttajatieto.id,
+      };
+    case "ADD_KAYTTAJA":
+      return {
+        ...state,
+        kayttajat: [action.payload, ...state.kayttajat],
+      };
+    case "EDIT_KAYTTAJA":
+      return {
+        ...state,
+        kayttajat: (state.kayttajat || []).map((kayttajatieto) =>
+          kayttajatieto && kayttajatieto.id === action.payload.id
+            ? { ...kayttajatieto, ...action.payload }
+            : kayttajatieto
+        ),
+      };
+    default:
+      return state;
+  }
+};

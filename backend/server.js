1f5d5e160f8ee667d1d037b9f3e6896493c7337a
Tomi Leminen <tleminen@student.uef.fi>
2024-03-27 09:39:52 +0200

    yritys saada toimimaan cors

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,8 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token"

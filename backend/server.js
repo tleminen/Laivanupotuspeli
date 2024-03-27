f3d7428b8df04b779e67b898ac58bcf350accd66
Tomi Leminen <tleminen@student.uef.fi>
2024-03-27 12:19:29 +0200

    lisäys server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use("/laivanupotus", require("./routes/postRoutes"));
 

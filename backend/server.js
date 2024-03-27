9a2f5d47a2127744d8cde2c50d41d7ab2b0d90a5
Tomi Leminen <tleminen@student.uef.fi>
2024-03-27 15:17:20 +0200

    muutosta

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,6 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
 
   res.setHeader(
     "Access-Control-Allow-Headers",
